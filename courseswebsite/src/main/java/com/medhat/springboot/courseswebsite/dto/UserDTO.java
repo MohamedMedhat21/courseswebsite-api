806fce1119a84e7eee99287707ba04283eb08613
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2023-08-16 09:49:10 +0300

    lombok for Users DTO

--- courseswebsite/src/main/java/com/medhat/springboot/courseswebsite/dto/UserDTO.java
+++ courseswebsite/src/main/java/com/medhat/springboot/courseswebsite/dto/UserDTO.java
@@ -1,5 +1,11 @@
 package com.medhat.springboot.courseswebsite.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserDTO {
 
     private Integer personalId;
@@ -7,53 +13,4 @@ public class UserDTO {
     private String email;
     private String role;
 
-    public UserDTO(Integer personalId, String userName, String email, String roleName) {
-        this.personalId = personalId;
-        this.userName = userName;
-        this.email = email;
-        this.role = roleName;
-    }
-
-    public Integer getPersonalId() {
-        return personalId;
-    }
-
-    public void setPersonalId(Integer personalId) {
-        this.personalId = personalId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "id=" + personalId +
-                ", userName='" + userName + '\'' +
-                ", email='" + email + '\'' +
-                ", roleName='" + role + '\'' +
-                '}';
-    }
 }
