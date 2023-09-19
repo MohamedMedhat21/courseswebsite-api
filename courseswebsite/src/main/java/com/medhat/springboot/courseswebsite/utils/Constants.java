c75d6f629fbcbf7091d30f5aa6119e77bc267be8
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2023-09-19 18:08:43 +0300

    jwt token time

--- courseswebsite/src/main/java/com/medhat/springboot/courseswebsite/utils/Constants.java
+++ courseswebsite/src/main/java/com/medhat/springboot/courseswebsite/utils/Constants.java
@@ -2,10 +2,11 @@ package com.medhat.springboot.courseswebsite.utils;
 
 public class Constants {
 
-    public static final int COURSE_PAGE_SIZE=15; // todo should be taken from fronend
-    public static final Integer DEFAULT_NEW_USER_ROLE=3;
+    public static final int COURSE_PAGE_SIZE=15; // todo should be taken from frontend
+    public static final Integer DEFAULT_NEW_USER_ROLE=3; // todo should be taken applied if it comes null from frontend
 
     public static final Integer DEFAULT_NEW_USER_ENABLED=1;
 
-    public static final int JWT_TOKEN_VALID_DURATION=60*50;
+    private static final Integer JWT_TOKEN_VALID_TIME_IN_MINUTES = 360;
+    public static final int JWT_TOKEN_VALID_DURATION = 60 * JWT_TOKEN_VALID_TIME_IN_MINUTES;
 }
